9eec9848230bfd9c72a5eb8e4a1399ec052a112c
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-04-27 15:47:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var katzDeliLine = []; 
 
- function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+  function takeANumber(katzDeli) {
+  number++
+  katzDeli.push(number)
+  return(`Welcome You are number ${number} in line.`);
 }
 
+var number = 0;
+
+
+
 function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   }
@@ -17,17 +17,9 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }	
 
-function currentLine() {
-  
-}
-
-
-
 var line = [];
 
-
-
- function currentLine(katzDeli) {
+function currentLine(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
     line.push(` `+[i+1]+`. `  + katzDeli[i])
